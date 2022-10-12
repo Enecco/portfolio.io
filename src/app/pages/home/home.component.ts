ce1da8a9166c5221cf589c8a72f2a6f9bcd60f82
Enecco Jacobelli <eneccojacobelli@gmail.com>
2022-10-11 20:25:31 -0400

    index

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,5 @@
 import { DataApiService } from './../../services/http/data-api.service';
 import { Component, OnInit, HostListener } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
 
 @Component({
   selector: 'app-home',
@@ -14,13 +12,11 @@ export class HomeComponent implements OnInit {
   featuredProjects;
   workExperience;
   profile;
-  CV;
 
   showMobileImages = false;
 
   constructor(
-    public dataApi: DataApiService,
-    private http: HttpClient
+    public dataApi: DataApiService
   ) { }
 
   @HostListener('window:resize', ['$event'])
@@ -38,6 +34,7 @@ export class HomeComponent implements OnInit {
   async ngOnInit(): Promise<void> {
     this.checkWindowSize();
 
+
     this.skills = await this.dataApi.getTopSkills();
     console.log('SKILLS', this.skills);
 
@@ -51,15 +48,11 @@ export class HomeComponent implements OnInit {
     this.profile = await this.dataApi.getProfile();
     console.log('PROFILE', this.profile);
 
-    const CVPromise = this.http.get('assets/media/base64/CV.json').toPromise();
-
-    Promise.all([CVPromise]).then(value => {
-      this.CV = value[0];
-    })
   }
 
   async downloadResume(): Promise<void> {
-    const byteCharacters = atob(this.CV);
+    const pdf: any =  await this.dataApi.getBase64CV();
+    const byteCharacters = atob(pdf.resume);
     const byteNumbers = new Array(byteCharacters.length);
     for (let i = 0; i < byteCharacters.length; i++) {
       byteNumbers[i] = byteCharacters.charCodeAt(i);
